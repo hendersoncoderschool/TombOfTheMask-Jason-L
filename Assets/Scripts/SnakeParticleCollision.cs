296d3e9b9b1dca7fd951bf566ad6d720d2078f3a
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2025-07-16 17:02:21 -0700

    snake level, particle code for multiple snakes

--- Assets/Scripts/SnakeParticleCollision.cs
+++ Assets/Scripts/SnakeParticleCollision.cs
@@ -4,46 +4,23 @@ using UnityEngine;
 
 public class SnakeParticleCollision : MonoBehaviour
 {
-    public GameObject SnakeHead;
-    public GameObject SnakeTail;
     public bool hitSnake;
     void Start()
     {
-        SnakeHead = GameObject.Find("SnakeHead");
-        SnakeTail = GameObject.Find("SnakeTail");
         GetComponent<ParticleSystem>().Stop();
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        print("collision");
-        if (other.gameObject == SnakeHead)
+        if (other.gameObject.name == "SnakeHead")
         {
-            //hitSnake = true;
             GetComponent<ParticleSystem>().Play();
-            print("hitsnakehead");
         }
     }
     void OnTriggerExit2D(Collider2D other)
     {
-        print("exitcollision");
-        if (other.gameObject == SnakeTail)
+        if (other.gameObject.name == "SnakeTail")
         {
-            //hitSnake = false;
             GetComponent<ParticleSystem>().Stop();
-            print("exitsnaketail");
-        }
-    }
-    void Update()
-    {
-        if (hitSnake)
-        {
-            //gameObject.GetComponent<ParticleSystem>().SetActive(true);
-            //GetComponent<ParticleSystem>().Stop();
-        }
-        else
-        {
-            //gameObject.GetComponent<ParticleSystem>().SetActive(false);
-            //GetComponent<ParticleSystem>().Play();
         }
     }
 }
