1e6d409188c87e160d52fbcc41425c9d58b467ef
Henderson The Coder School <hendersoncoderschool@gmail.com>
2025-09-10 17:00:43 -0700

    tried to extract data from save

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MiniJSON;
+using System.IO;
+[System.Serializable]
+public class GameData
+{
+    public int FurthestLevel;
+    public int[] Stars;
+}
+
 public class LevelManager : MonoBehaviour
 {
     public int FurthestLevel;
@@ -16,10 +24,15 @@ public class LevelManager : MonoBehaviour
         FurthestLevel = 0;
 
         string saveFile = Path.Combine(Application.persistentDataPath, "save.json");
-        if (saveFile.Exists(saveFile))
+        if (File.Exists(saveFile))
         {
-            string json = saveFile.ReadAllText(saveFile);
-            
+            string json = File.ReadAllText(saveFile);
+            GameData loadedGameData = JsonUtility.FromJson<GameData>(json);
+            print(loadedGameData.Stars);
+    
+            //FurthestLevel = int.Parse(json.Substring(24));
+
+            //print(FurthestLevel);
         }
         DontDestroyOnLoad(gameObject);
     }
